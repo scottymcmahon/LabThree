f15ed350e0a1f2658100141f16eb5e9a16eebda0
scottymcmahon <scottmcmahondev@outlook.com>
2016-10-17 13:51:50 +0100

    Initial commit

---
+++ helloPrinter.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ScottyMcMahon
+ * Date: 17/10/2016
+ * Time: 13:36
+ */
---
+++ helloSelector.php
@@ -0,0 +1,10 @@
+<a>
+    Where would you like to say hello?
+</a>
+
+<ul>
+    <?php
+        echo "<a href='helloPrinter.php?planet = earth'> Earth</a>"
+        echo "<a> sdcsdc</a>
+    ?>
+</ul>
