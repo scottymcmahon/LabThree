3b55c9299345454d0ce675574d0877f067551907
scottymcmahon <scottmcmahondev@outlook.com>
2016-10-17 14:09:35 +0100

    1.1

--- helloSelector.php
+++ helloSelector.php
@@ -1,10 +1,13 @@
-<a>
+<!DOCTYPE html>
+<html lang = "en">
+    <a>
     Where would you like to say hello?
-</a>
+    </a>
 
-<ul>
-    <?php
-        echo "<a href='helloPrinter.php?planet = earth'> Earth</a>"
-        echo "<a> sdcsdc</a>
-    ?>
-</ul>
+    <ul>
+        <?php
+            echo "<a href='helloPrinter.php?planet = earth'> Earth</a>";
+            echo "<a> sdcsdc</a>";
+        ?>
+    </ul>
+</html>
